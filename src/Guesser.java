54b2aa9133441f863ce17af31c57a45189f77a25
sophie126 <sojopa800@gmail.com>
2024-11-23 22:00:21 -0500

    Work from home

--- src/Guesser.java
+++ src/Guesser.java
@@ -1,16 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Guesser {
-    private int guess;
+private int guess;
+private int newGuess;
 
 
-    public Guesser() {
-        guess = (int) (Math.random() * 99) + 1;
+
+
+public Guesser(int min, int max) {
+    guess = (int) (Math.random() * (max - min + 1) + min);
+}
+
+public int returnGuess() {
+    return guess;
+}
+
+
+public int tooHighOrLow(String lowOrHigh, int min, int max) {
+    newGuess = (int) (Math.random() * (max - min + 1) + min);
+    if (newGuess == guess && lowOrHigh.equals("too low")) {
+        newGuess++;
     }
-
-
-    public void printGuess() {
-        System.out.print("Is " + guess + " your number?");
+    if (newGuess == guess && lowOrHigh.equals("too high")) {
+        newGuess--;
     }
+    guess = newGuess;
+    return newGuess;
+}
+//make sure there are no repeats
+
+
+public void printGuess() {
+    System.out.print("Is " + guess + " your number? Enter \"yes\", \"too low\", or \"too high\"");
+}
+
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,30 +1,50 @@
 import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
-        String useBoolean = "Answer in true or false";
+        int newGuess = 0;
+        int count = 1;
+
+
         Scanner userInput = new Scanner(System.in);
-        System.out.println(useBoolean + "\n");
-        System.out.println("Think of a number 1 - 100 \n");
-        Guesser originalGuess = new Guesser();
+        System.out.println("What should the minimum be?");
+        int min = userInput.nextInt();
+        System.out.println("What should the maximum be?");
+        int max = userInput.nextInt();
+        Guesser originalGuess = new Guesser(min, max);
+        newGuess = originalGuess.returnGuess();
         System.out.println("Press enter when done");
         String doneOrNot = userInput.nextLine();
 
 
-        try {
-            originalGuess.printGuess();
-            boolean wrongOrRight = userInput.nextBoolean();
-            if (wrongOrRight) {
-                System.out.println("YAY");
-            }
-            if (!wrongOrRight) {
-                System.out.println("ok");
+        originalGuess.printGuess();
+        String wrongOrRight = userInput.nextLine();
 
+
+        while (!wrongOrRight.equals("yes")) {
+            if (wrongOrRight.equals("too low") || wrongOrRight.equals("too high")) {
+                count++;
+                if (wrongOrRight.equals("too low")) {
+                    min = newGuess;
+                }
+                if (wrongOrRight.equals("too high")) {
+                    max = newGuess;
+                }
+                newGuess = originalGuess.tooHighOrLow(wrongOrRight, min, max);
+                originalGuess.printGuess();
+                wrongOrRight = userInput.nextLine();
+            } else {
+                System.out.println("Enter \"yes\", \"too low\", or \"too high\"");
+                wrongOrRight = userInput.nextLine();
             }
         }
-        catch(Exception e) {
-            System.out.println(useBoolean);
+
+        String congrats = "That took " + count + " tries";
+        if (count < 10) {
+            congrats += ". Wow! I did better than I though I would.";
+        } else {
+            count--;
+            congrats += ". I kinda suck at this ngl. Next time I'll try to get it down to " + count + ".";
         }
-
-
+        System.out.println(congrats);
     }
 }
